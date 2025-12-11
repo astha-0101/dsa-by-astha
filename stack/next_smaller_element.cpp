106cf6386d9538f092d1eb620b036f886a26f8c1
astha-0101 <asthasingh01224@gmail.com>
2025-12-11 07:33:10 +0530

    Add files via upload

---
+++ stack/next_smaller_element.cpp
@@ -0,0 +1,22 @@
+#include <stack>
+#include <vector>
+using namespace std;
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    vector<int> ans(n);
+    stack <int> s;
+    for(int i=n-1;i>=0;i--){
+        while(!s.empty() && s.top()>=arr[i]){
+            s.pop();
+        }
+        if(s.empty()){
+            ans[i]=-1;
+        }
+        else{
+            ans[i]=s.top();
+        }
+        s.push(arr[i]);
+    }
+    return ans;
+    // Write your code here.
+}
