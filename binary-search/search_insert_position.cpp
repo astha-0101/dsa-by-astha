3683185a2c2310506b620199dde25185785e09b1
astha-0101 <asthasingh01224@gmail.com>
2025-12-17 05:43:42 +0530

    Add files via upload

---
+++ binary-search/search_insert_position.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int mid=0;
+         while(low <= high) {
+        int mid = (low + high) / 2;
+      
+        if(nums[mid] == target) {
+            return mid;   // found
+        }
+        else if(nums[mid] < target) {
+            low = mid + 1;   // go right
+        }
+        else {
+            high = mid - 1;  // go left
+        }
+    }
+
+    return low; // not found
+}
+        
+    
+};
